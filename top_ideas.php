7678355b96c9d64c7f1e27731ce13d3676e4e9b7
lamaGH2030 <ghlama579@gmail.com>
2026-08-05 16:14:58 +0300

    Delete top_ideas.php

--- top_ideas.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/includes/bootstrap.php';
-require_admin();
-$page = 'top';
-$pageTitle = 'أفضل الأفكار | منصة الأفكار الذكية';
-require __DIR__ . '/includes/layout_top.php';
-require __DIR__ . '/pages/top.php';
-require __DIR__ . '/includes/layout_bottom.php';
