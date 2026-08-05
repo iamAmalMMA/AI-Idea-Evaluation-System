76217ade25b38eb6860f8607faa73e606f6e3de2
lamaGH2030 <ghlama579@gmail.com>
2026-08-05 16:16:07 +0300

    Delete ideas.php

--- ideas.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/includes/bootstrap.php';
-$page = 'ideas';
-$pageTitle = 'الأفكار | منصة الأفكار الذكية';
-require __DIR__ . '/includes/layout_top.php';
-require __DIR__ . '/pages/ideas.php';
-require __DIR__ . '/includes/layout_bottom.php';
