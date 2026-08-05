ce17fbffc60f58588c68b068df248366857fcf2d
lamaGH2030 <ghlama579@gmail.com>
2026-08-05 16:15:36 +0300

    Delete analytics.php

--- analytics.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/includes/bootstrap.php';
-$page = 'analytics';
-$pageTitle = 'الإحصائيات | منصة الأفكار الذكية';
-require __DIR__ . '/includes/layout_top.php';
-require __DIR__ . '/pages/analytics.php';
-require __DIR__ . '/includes/layout_bottom.php';
