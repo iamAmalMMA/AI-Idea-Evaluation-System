99465ec10850d301e103bcc923200618790649e3
lamaGH2030 <ghlama579@gmail.com>
2026-08-05 16:16:38 +0300

    Delete new_idea.php

--- new_idea.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/includes/bootstrap.php';
-if (is_admin()) { flash('إضافة الأفكار متاحة للموظفين فقط'); redirect(url('dashboard')); }
-$page = 'new';
-$pageTitle = 'إضافة فكرة | منصة الأفكار الذكية';
-require __DIR__ . '/includes/layout_top.php';
-require __DIR__ . '/pages/new.php';
-require __DIR__ . '/includes/layout_bottom.php';
